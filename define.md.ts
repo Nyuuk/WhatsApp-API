4f5e92496bd800d78bbebffc09fa5465810f0df5
Adnan <dnanfab@gmail.com>
2024-02-10 19:08:11 +0700

    update: adding ResponseHelper

---
+++ define.md.ts
@@ -0,0 +1,4 @@
+export interface dataMessageInterface {
+    text: string
+    number: number
+}
--- index.ts
+++ index.ts
@@ -2,91 +2,99 @@ import dotenv from "dotenv";
 import express from "express";
 import WhatsApp from "./helpers/WhapsApp";
 import crypto from "crypto";
+import { dataMessageInterface } from "./define.md";
+import ResponseHelper from "./helpers/ResponseHelper";
 
 dotenv.config();
 
-// const WA = new WhatsApp();
-// WA.makeConnection();
+const WA = new WhatsApp();
+WA.makeConnection();
 
 const api = express();
 api.use(express.json());
 api.use(express.urlencoded({ extended: true }));
 
 // middlewate
-api.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
-    const api_key = req.get('x-api-key');
-    if (!api_key) {
-        res.status(404).json({
-            message: "x-api-key is required",
-            status: "error",
-        })
-        return
+api.use(
+    (
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction
+    ) => {
+        const api_key = req.get("x-api-key");
+        if (!api_key) {
+            ResponseHelper(res, "x-api-key is required", 404)
+            return;
+        }
+        if (api_key !== process.env.X_API_KEY) {
+            ResponseHelper(res, "x-api-key is invalid", 404)
+            return;
+        }
+        const secret = req.get("x-api-secret");
+        if (!secret) {
+            ResponseHelper(res, "x-api-secret is required", 404)
+            return;
+        }
+        const hasSecret = crypto
+            .createHash("sha256")
+            .update(
+                process.env.X_API_KEY +
+                    req.originalUrl +
+                    Math.floor(Date.now() / 1000)
+            )
+            .digest("hex");
+        if (secret !== hasSecret) {
+            ResponseHelper(res, "x-api-secret is invalid", 404)
+            return;
+        }
+        if (req.body.timestamp !== Math.floor(Date.now() / 1000)) {
+            ResponseHelper(res, "timestamp is invalid", 404)
+            return;
+        }
+        next();
     }
-    if (api_key !== process.env.X_API_KEY) {
-        res.status(404).json({
-            message: "x-api-key is invalid",
-            status: "error",
-        })
-        return
-    }
-    const secret= req.get('x-api-secret');
-    if (!secret) {
-        res.status(404).json({
-            message: "x-api-secret is required",
-            status: "error",
-        })
-        return;
-    }
-    const hasSecret = crypto.createHash('sha256').update(process.env.X_API_KEY + req.originalUrl + Math.floor(Date.now() / 1000)).digest('hex');
-    if (secret !== hasSecret) {
-        res.status(404).json({
-            message: "x-api-secret is invalid",
-            status: "error",
-        })
-        return
-    }
-    if (req.body.timestamp !== Math.floor(Date.now() / 1000)) {
-        res.status(404).json({
-            message: "timestamp is invalid",
-            status: "error",
-        })
-        return
-    }
-    next();
-})
-
-api.get("/ping", (req, res) => {
-    res.send("pong");
-});
-api.post("/test", (req, res) => {
-    const headers = req.headers;
-    const body = req.body;
-    res.send({
-        headers,
-        body,
-    });
-});
+);
 
 api.post("/send-message", async (req: express.Request, res) => {
     const json = req.body;
     if (!json.text) {
-        res.status(404).json({
-            message: "text is required",
-            status: "error",
-            body: json,
-        });
-        return;
+        ResponseHelper(res, 'text is required', 404)
+        return
     }
     if (!json.number) {
-        res.status(404).json({
-            message: "number is invalid && start with 62",
-            status: "error",
-            body: json,
-        });
+        ResponseHelper(res, 'number is invalid & start with 62', 404)
+        return
+    }
+    const r = await WA.sendText(json.number, json.text);
+    ResponseHelper(res, r)
+});
+
+api.post("/send-some-messages", async (req: express.Request, res) => {
+    const json = req.body;
+    if (!json.data || !json.data.length) {
+        ResponseHelper(res, 'data is required & array', 404)
         return;
     }
-    // const r = await WA.sendText(json.number, json.text);
-    res.json({ message: "success" });
+    // looping json.data
+    const dataError: dataMessageInterface[] = [];
+    const dataSuccess: { number: number }[] = [];
+    json.data.forEach(async (data: dataMessageInterface) => {
+        if (!data.text) {
+            dataError.push(data);
+            return;
+        }
+        if (!data.number) {
+            dataError.push(data);
+            return;
+        }
+        const r = await WA.sendText(data.number, data.text);
+        if (!r) {
+            dataError.push(data);
+            return;
+        }
+        dataSuccess.push({ number: data.number });
+    });
+    ResponseHelper(res, { dataError, dataSuccess }, 200)
 });
 
 api.listen(3000, async () => {
