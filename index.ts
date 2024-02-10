ba1386c6c3d3aafce2bcac3f045e3ad611d0f0d9
Adnan <dnanfab@gmail.com>
2024-02-10 17:45:31 +0700

    update: adding middleware

--- index.ts
+++ index.ts
@@ -1,15 +1,60 @@
 import dotenv from "dotenv";
 import express from "express";
 import WhatsApp from "./helpers/WhapsApp";
+import crypto from "crypto";
+import { HeadersInterface } from "./define.md";
 
 dotenv.config();
 
-const WA = new WhatsApp();
-WA.makeConnection();
+// const WA = new WhatsApp();
+// WA.makeConnection();
 
 const api = express();
 api.use(express.json());
 api.use(express.urlencoded({ extended: true }));
+
+// middlewate
+api.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const api_key = req.get('x-api-key');
+    if (!api_key) {
+        res.status(404).json({
+            message: "x-api-key is required",
+            status: "error",
+        })
+        return
+    }
+    if (api_key !== process.env.X_API_KEY) {
+        res.status(404).json({
+            message: "x-api-key is invalid",
+            status: "error",
+        })
+        return
+    }
+    const secret= req.get('x-api-secret');
+    if (!secret) {
+        res.status(404).json({
+            message: "x-api-secret is required",
+            status: "error",
+        })
+        return;
+    }
+    const hasSecret = crypto.createHash('sha256').update(process.env.X_API_KEY + req.originalUrl).digest('hex');
+    if (secret !== hasSecret) {
+        res.status(404).json({
+            message: "x-api-secret is invalid",
+            status: "error",
+        })
+        return
+    }
+    if (req.body.timestamp !== Math.floor(Date.now() / 1000)) {
+        res.status(404).json({
+            message: "timestamp is invalid",
+            status: "error",
+        })
+        return
+    }
+    next();
+})
 
 api.get("/ping", (req, res) => {
     res.send("pong");
@@ -24,42 +69,7 @@ api.post("/test", (req, res) => {
 });
 
 api.post("/send-message", async (req: express.Request, res) => {
-    const now = Math.floor(Date.now() / 1000);
     const json = req.body;
-    const headers = req.headers;
-    if (!headers["x-api-key"]) {
-        res.status(404).json({
-            message: "x-api-key is required",
-            status: "error",
-        });
-        return;
-    }
-    if (headers["x-api-key"] != process.env.X_API_KEY) {
-        res.status(404).json({
-            message: "x-api-key is invalid",
-            status: "error",
-        });
-        return;
-    }
-
-    if (!Object.keys(json).includes("timestamp")) {
-        res.status(404).json({
-            message: "timestamp is required",
-            timestamp: now,
-            status: "error",
-            body: json,
-        });
-        return;
-    }
-    if (json.timestamp != now) {
-        res.status(404).json({
-            message: "timestamp is invalid",
-            timestamp: now,
-            status: "error",
-            body: json,
-        });
-        return;
-    }
     if (!json.text) {
         res.status(404).json({
             message: "text is required",
@@ -76,8 +86,8 @@ api.post("/send-message", async (req: express.Request, res) => {
         });
         return;
     }
-    const r = await WA.sendText(json.number, json.text);
-    res.json({ r });
+    // const r = await WA.sendText(json.number, json.text);
+    res.json({ message: "success" });
 });
 
 api.listen(3000, async () => {
